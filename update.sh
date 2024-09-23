625986d2eaceb4f3c9e921aaf1cf51aa61b03e2d
Sameep <sameepsk2@gmail.com>
2024-09-23 14:27:33 +0545

    2024-09-23

--- update.sh
+++ update.sh
@@ -6,4 +6,6 @@ echo "Copying waybar"
 cp -r ~/.config/waybar/ ./
 echo "Copying zshrc and bashrc"
 cp -r ~/.zshrc ~/.bashrc ./SHELL/
+echo "Copying ranger"
+cp -r ~/.config/ranger/ ./
 echo "Copy Finished"

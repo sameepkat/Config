4ea52276b6c07f35801526f3b6d2d32f42decdc4
Sameep <sameepsk2@gmail.com>
2024-10-08 10:19:28 +0545

    2024-10-08

---
+++ nvim/lua/options.lua
@@ -0,0 +1,16 @@
+-- nvim/lua/options.lua
+--
+--
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.autoindent = true
+vim.o.clipboard = 'unnamedplus'
+vim.o.relativenumber = true
+
+vim.o.tabstop = 3
+vim.o.shiftwidth = 3
+vim.o.updatetime = 300
+
+vim.o.termguicolors = true
+
+vim.o.mouse = 'a'
---
+++ nvim/lua/pluginlist.lua
@@ -0,0 +1,66 @@
+return{
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = {
+			"nvim-tree/nvim-web-devicons"
+		},
+		config = function()
+			require("lualine").setup({
+				icons_enable = true,
+			})
+		end,
+},
+{
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+
+	'folke/neodev.nvim',
+},
+{
+	'hrsh7th/nvim-cmp',
+	dependencies = {
+		'L3MON4D3/LuaSnip',
+		'saadparwaiz1/cmp_luasnip',
+		'rafamadriz/friendly-snippets',
+		'hrsh7th/cmp-nvim-lsp',
+	},
+},
+{
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate',
+},
+{
+	'nvim-telescope/telescope-fzf-native.nvim',
+	build = 'make',
+	dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' }
+},
+	{
+		"rose-pine/neovim", name = "rose-pine"
+	},
+	{
+		"phaazon/hop.nvim",
+		config = function ()
+			require('hop').setup()
+		end,
+	},
+	{
+        "MunifTanjim/prettier.nvim",
+        config = function()
+            require('prettier').setup({
+                bin = 'prettierd', -- or `'prettier'` for direct usage
+                filetypes = {
+                    "javascript", "typescript", "css", "html", "json",
+                    "yaml", "markdown", "graphql", "javascriptreact",
+                    "typescriptreact"
+                }
+            })
+        end
+    },
+}

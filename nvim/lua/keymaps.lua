af364417d268f6c9139d3c1426def86d3e2f579f
Sameep <sameepsk2@gmail.com>
2024-11-27 20:56:43 +0545

    2024-11-27

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,3 +1,8 @@
 vim.api.nvim_set_keymap('i', 'jj', '<Esc>', {noremap = true, silent=true})
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.api.nvim_set_keymap('n', '<leader>n', ':bnext<CR>', {noremap = true, silent=true})
+vim.api.nvim_set_keymap('n', '<leader>p', ':bprevious<CR>', {noremap = true, silent=true})
+vim.api.nvim_set_keymap('n', '<leader>bd', ':bdelete<CR>', {noremap = true, silent=true})
+
+
 
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -34,71 +34,74 @@ return {
 		ft = "java",
 	},
 	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-		},
-		config = function()
-			local lspconfig = require('lspconfig')
-			require("mason-lspconfig").setup({
-				ensure_installed = { "jdtls" }
-			})
-			
-			-- Configure Java LSP
-			lspconfig.jdtls.setup({
-				capabilities = require('cmp_nvim_lsp').default_capabilities(),
-				cmd = { 'jdtls' },
-				root_dir = lspconfig.util.root_pattern(".git", "mvnw", "gradlew", "pom.xml", "build.gradle"),
-			})
-		end
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"L3MON4D3/LuaSnip",
-		},
-		config = function()
-			local cmp = require('cmp')
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require('luasnip').lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' },
-				}),
-			})
-		end
-	},
-	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
 		config = function()
 			require("nvim-autopairs").setup({
 				check_ts = true, -- Enable treesitter
 				ts_config = {
-					lua = {'string'},-- Don't add pairs in lua string treesitter nodes
-					javascript = {'template_string'},-- Don't add pairs in javascript template_string
+					lua = {'string'},
+					javascript = {'template_string'},
 				}
 			})
-			
-			-- If you want to automatically add `(` after selecting a function or method
-			local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-			local cmp = require('cmp')
-			cmp.event:on(
-				'confirm_done',
-				cmp_autopairs.on_confirm_done()
-			)
 		end
 	},
 	{
 		'wakatime/vim-wakatime', lazy = false
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+		end
+	},
+	{
+		'neoclide/coc.nvim',
+		branch = 'release',
+		build = 'npm ci',
+		config = function()
+			-- Set highlight groups for coc.nvim
+			vim.api.nvim_set_hl(0, 'CocFloating', { bg = '#1f2335' })
+			vim.api.nvim_set_hl(0, 'CocMenuSel', { bg = '#2d3343' })
+			
+			-- Configure floating window border
+			vim.g.coc_borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' }
+			
+			-- Set pumblend for transparency
+			vim.opt.pumblend = 10
+
+			-- Use arrow keys for navigating completion
+			vim.keymap.set('i', '<Down>', function()
+				return vim.fn['coc#pum#visible']() == 1 and vim.fn['coc#pum#next'](1) or '<Down>'
+			end, { expr = true })
+			
+			vim.keymap.set('i', '<Up>', function()
+				return vim.fn['coc#pum#visible']() == 1 and vim.fn['coc#pum#prev'](1) or '<Up>'
+			end, { expr = true })
+
+			-- Use Enter to confirm completion
+			vim.keymap.set('i', '<CR>', function()
+				return vim.fn['coc#pum#visible']() == 1 and vim.fn['coc#pum#confirm']() or '<CR>'
+			end, { expr = true })
+
+			-- Use K to show documentation in preview window
+			function _G.show_docs()
+				local cw = vim.fn.expand('<cword>')
+				if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+					vim.api.nvim_command('h ' .. cw)
+				elseif vim.api.nvim_eval('coc#rpc#ready()') then
+					vim.fn.CocActionAsync('doHover')
+				else
+					vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+				end
+			end
+			vim.keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+		end
 	}
 }
